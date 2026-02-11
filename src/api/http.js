06148bc1a6bb2cff56a4ef4eacf70eec4d91b2ab
Shahbaj Taher <shahbajtahershezan@gmail.com>
2026-02-11 15:13:34 +0600

    Update http.js

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://contact-app-shahbaj.netlify.app/",
   headers: { "Content-Type": "application/json" }
 });
